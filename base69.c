87457fe83ec015c5bb6ca0aea3757f5dca15a7f9
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-12-03 09:05:12 +0000

    Base 69 encoder

--- base69.c
+++ base69.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "base69.h"
 #include <string.h>
 
 // Base69 alphabet
@@ -41,13 +42,3 @@ void encode_base69(int input, char *output) {
     strcat(output, tempBuffer); // Concatenate the reversed string to the output after the prefix
 }
 
-int main() {
-    int input = 100;
-    char output[24]; // Allocate enough space for the output including the prefix
-
-    encode_base69(input, output);
-
-    printf("Encoded: %s\n", output);
-
-    return 0;
-}
---
+++ base69.h
@@ -0,0 +1,7 @@
+#ifndef BASE69_H
+#define BASE69_H
+
+// Function to encode an integer into base69 with a prefix
+void encode_base69(int input, char *output);
+
+#endif /* BASE69_H */
