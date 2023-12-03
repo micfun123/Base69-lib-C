5d02ab6123bd3346a71536f3657894171f915e1d
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-12-03 08:52:01 +0000

    base69 encoding

---
+++ base69.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Base69 alphabet
+const char *base69_alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/=@*-!#";
+
+// Reverse a string
+void reverseString(char *str) {
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+// Base69 encoder
+void encode_base69(int input, char *output) {
+    int remainder;
+    int index = 0;
+    char tempBuffer[input]; // Assuming a maximum length for the encoded string
+
+    // Add prefix "69*|"
+    strcpy(output, "69*|");
+    int prefixLength = strlen(output);
+
+    while (input > 0) {
+        remainder = input % 70;
+        input /= 70;
+        tempBuffer[index++] = base69_alphabet[remainder];
+    }
+
+    tempBuffer[index] = '\0'; // Null-terminate the temporary buffer
+    reverseString(tempBuffer); // Reverse the encoded string
+
+    strcat(output, tempBuffer); // Concatenate the reversed string to the output after the prefix
+}
+
+int main() {
+    int input = 100;
+    char output[24]; // Allocate enough space for the output including the prefix
+
+    encode_base69(input, output);
+
+    printf("Encoded: %s\n", output);
+
+    return 0;
+}
